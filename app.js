61128dc7958a21ed433b32a300483a86ab586930
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-03-08 23:12:26 +0530

    Added map api

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ var express = require('express'),
     app = express(),
     PORT = 8000 | process.env.PORT
 
+app.set('view engine','ejs');
 app.use(express.static('assets'))
 
-app.get("/", function(req, res){
-    res.send("Home page")
+app.get('/', function(req, res){
+    res.render('home')
+})
+
+app.get('/booths/nearme', function(req, res){
+    res.render('nearme')
 })
 
 app.listen(PORT, function(err){
     if(err)
         console.log(err)
     else
-        console.log("Server has started...")
+        console.log('Server has started...')
 })
