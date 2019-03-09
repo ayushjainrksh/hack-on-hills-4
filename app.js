d24f5857de8f0c2ccfdcb17a78b6ad2781cdc1c6
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-03-09 14:22:42 +0530

    Marking locations using Place API

--- app.js
+++ app.js
@@ -3,15 +3,112 @@ require('dotenv').config()
 var express = require('express'),
     app = express(),
     PORT = 4000 | process.env.PORT
+    fs = require('fs');
+    csv = require('csv-parse');
 
 app.set('view engine','ejs');
 app.use(express.static('assets'))
+
+API_KEY=process.env.API_KEY
+app.use(function(req, res, next){
+  res.locals.API_KEY = req.API_KEY;
+  next();
+});
+
+// database/hamirpur_polling_booth.tsv
+
+// var file = fs.readFileSync("/database/hamirpur_polling_booth.tsv");
+// fs.readFile("/database/hamirpur_polling_booth.tsv", {encoding: 'utf-8'}, function(err,data){
+//   if (!err) {
+//       console.log('received data: ' + data);
+//       response.writeHead(200, {'Content-Type': 'text/html'});
+//       response.write(data);
+//       response.end();
+//   } else {
+//       console.log(err);
+//   }
+// });
+
+
+// var lineReader = require('readline').createInterface({
+//   input: require('fs').createReadStream('/database/hamirpur_polling_booth.tsv')
+// });
+
+// lineReader.on('line', function (line) {
+//   console.log('Line from file:', line);
+// });
+
+
+// app.get("/hidden", function(req, res){
+//   fs.readFile('/database/hamirpur_polling_booth.tsv', function(err, data) {
+//     res.writeHead(200);
+//     res.write(data);
+//     res.end();
+//   });
+// })
+
+var fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join(__dirname, '/assets/database/hamirpur_polling_booth.tsv');
+
+app.get('/hidden', function(req, res){
+  // console.log("In hidden")
+    var arr = []
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+      // console.log(filePath)
+      if (err) {
+
+        console.log(err);
+      } else {
+        // console.log('received data: ' + data);
+        
+        res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          res.end();
+      }
+  });
+
+})
+
+// csv(file, {delimiter: tab, relax: true, columns: true}, (err, out) => {
+//     if (err) {
+//       console.log("ERROR: " + err.message);
+//     }
+//     if (out) {
+//       out = _.sortBy(out, function (film) {
+//         return film.Year;
+//       })
+  
+//       _.each(out, function (film) {
+//         var runtime = film.Runtime.length == 0 ? null : parseInt(film.Runtime, 10);
+//         if (result[film.Year]) {
+//           result[film.Year].push(runtime);
+//         } else {
+//           result[film.Year] = [runtime]
+//         }
+//       });
+  
+//       _.each(result, (runtimes, year) => {
+//         var onlyValues = _.filter(runtimes, (num) => { return !_.isNull(num); });
+//         ranges.push([year, Math.min.apply(null, onlyValues), Math.max.apply(null, onlyValues),]);
+//         averages.push([year, Math.round(_.reduce(onlyValues, (memo, num) => {
+//           return memo + num;
+//         }, 0)/onlyValues.length)])
+//       });
+  
+//       fs.writeFile('averages.json', JSON.stringify(averages, null, 2));
+//       fs.writeFile('ranges.json', JSON.stringify(ranges, null, 2));
+//     }
+//   });
+
+
 
 app.get('/', function(req, res){
     res.render('home')
 })
 
 app.get('/booths/nearme', function(req, res){
+    // console.log(process.env.API_KEY)
     res.render('nearme',{API_KEY : process.env.API_KEY})
 })
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,95 @@
 var map, infoWindow;
-var setZoom = 4.77;
+var service;
+// var setZoom = 4.77;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 23.1339722, lng: 77.7507368},
         zoom: 4.77
     });
-    infoWindow = new google.maps.InfoWindow;
 
-    // Try HTML5 geolocation.
+    // Geolocation
+    infoWindow = new google.maps.InfoWindow();
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
+        
+        // Mark my location
+        var locMarker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            title: 'Im here'
+          });
+          locMarker.addListener('click', function() {
+              infoWindow.setContent(place.name);
+              infowindow.open(map, locMarker);
+          });
+  
+        // infoWindow.setPosition(pos);
+        // infoWindow.setContent('Location found.');
+        // infoWindow.open(map);
         map.setCenter(pos);
         }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
+            handleLocationError(true, infoWindow, map.getCenter());
         });
     } else {
-        // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
+
+    // Marker
+    var request = [
+        {
+        query: 'G.S.S.S., AMROH',
+        fields: ['name', 'geometry'],
+        },{
+        query: 'G.P.S. DARBELI',
+        fields: ['name', 'geometry']
+        }
+    ];
+
+
+
+    // var features = [
+    //     {
+    //       position: new google.maps.LatLng(-33.91721, 151.22630),
+    //       type: 'info'
+    //     }, {
+    //       position: new google.maps.LatLng(-33.91539, 151.22820),
+    //       type: 'info'
+    //     }
+
+
+    service = new google.maps.places.PlacesService(map);
+    request.forEach(function(requestPlace){
+        service.findPlaceFromQuery(requestPlace, function(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+    
+            map.setCenter(results[0].geometry.location);
+            }
+        });
+    });
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  }
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(place.name);
+        infoWindow.open(map, this);
+    });
+}
